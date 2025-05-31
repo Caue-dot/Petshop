f42754c96dc107b99bbf4328cf0da00835fa2933
Caue <cauecvx@gmail.com>
2025-05-31 05:42:17 -0300

    Adicionado helpers
    com comando print_console para ajudar a debugar

---
+++ helpers/php_helper.php
@@ -0,0 +1,8 @@
+<?php
+function print_console($data) {
+    $output = $data;
+    if (is_array($output))
+        $output = implode(',', $output);
+
+    echo "<script>console.log('Debug Objects: " . $output . "' );</script>";
+}
